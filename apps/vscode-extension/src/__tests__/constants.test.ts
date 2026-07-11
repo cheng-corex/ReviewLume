7b027f909522e81a3e46fd5aa0156ddc627c255f
cheng-corex <415944341@qq.com>
2026-07-11 11:47:21 +0800

    test: cover P4/P5 command constants

--- apps/vscode-extension/src/__tests__/constants.test.ts
+++ apps/vscode-extension/src/__tests__/constants.test.ts
@@ -3,56 +3,28 @@ import { COMMANDS, VIEWS, OUTPUT_CHANNEL_NAME } from '../constants';
 
 describe('constants', () => {
   describe('COMMANDS', () => {
-    it('should define the hello command', () => {
-      expect(COMMANDS.HELLO).toBe('reviewlume.hello');
-    });
-
-    it('should define the createReviewPack command', () => {
-      expect(COMMANDS.CREATE_REVIEW_PACK).toBe('reviewlume.createReviewPack');
-    });
-
-    it('should define the addRelatedFiles command', () => {
-      expect(COMMANDS.ADD_RELATED_FILES).toBe('reviewlume.addRelatedFiles');
-    });
-
-    it('should define the recommendTestFiles command', () => {
-      expect(COMMANDS.RECOMMEND_TEST_FILES).toBe('reviewlume.recommendTestFiles');
-    });
-
-    it('should define the openReviewHistory command', () => {
-      expect(COMMANDS.OPEN_REVIEW_HISTORY).toBe('reviewlume.openReviewHistory');
-    });
-
-    it('should define the importReviewResponse command', () => {
-      expect(COMMANDS.IMPORT_REVIEW_RESPONSE).toBe('reviewlume.importReviewResponse');
-    });
-
-    it('should have all command IDs as const (no extra properties)', () => {
-      const keys = Object.keys(COMMANDS);
-      expect(keys).toEqual([
-        'HELLO',
-        'CREATE_REVIEW_PACK',
-        'ADD_RELATED_FILES',
-        'RECOMMEND_TEST_FILES',
-        'OPEN_REVIEW_HISTORY',
-        'IMPORT_REVIEW_RESPONSE',
-      ]);
+    it('defines every ReviewLume command ID', () => {
+      expect(COMMANDS).toEqual({
+        HELLO: 'reviewlume.hello',
+        CREATE_REVIEW_PACK: 'reviewlume.createReviewPack',
+        ADD_RELATED_FILES: 'reviewlume.addRelatedFiles',
+        RECOMMEND_TEST_FILES: 'reviewlume.recommendTestFiles',
+        SCAN_SELECTED_FILES: 'reviewlume.scanSelectedFiles',
+        EXPORT_REVIEW_PACK: 'reviewlume.exportReviewPack',
+        OPEN_REVIEW_HISTORY: 'reviewlume.openReviewHistory',
+        IMPORT_REVIEW_RESPONSE: 'reviewlume.importReviewResponse',
+      });
     });
   });
 
   describe('VIEWS', () => {
-    it('should define the view container ID', () => {
+    it('defines the view IDs', () => {
       expect(VIEWS.CONTAINER).toBe('reviewlume');
-    });
-
-    it('should define the main view ID', () => {
       expect(VIEWS.MAIN_VIEW).toBe('reviewlume.mainView');
     });
   });
 
-  describe('OUTPUT_CHANNEL_NAME', () => {
-    it('should be "ReviewLume"', () => {
-      expect(OUTPUT_CHANNEL_NAME).toBe('ReviewLume');
-    });
+  it('uses the ReviewLume output channel name', () => {
+    expect(OUTPUT_CHANNEL_NAME).toBe('ReviewLume');
   });
 });
